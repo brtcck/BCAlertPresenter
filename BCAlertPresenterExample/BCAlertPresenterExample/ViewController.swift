c98e76e7a6fc18e2acc3ca5d7377a541469a5c38
Berat Çiçek <berat@vngrs.com>
2018-10-13 20:24:25 +0300

    Implement alert presenter

--- BCAlertPresenterExample/BCAlertPresenterExample/ViewController.swift
+++ BCAlertPresenterExample/BCAlertPresenterExample/ViewController.swift
@@ -10,16 +10,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    private let alertPresenter = BCAlertPresenter()
+}
+
+// MARK: - Actions
+
+extension ViewController {
+
+    @IBAction func didTapPressButton(_ sender: UIButton) {
+
+        /// Okay button handler
+        let okActionHandler = { (action: UIAlertAction) in
+
+            print("OK button tapped!")
+        }
+
+        /// Okay action
+        let okAction = UIAlertAction(
+            title: "OK",
+            style: .default,
+            handler: okActionHandler
+        )
+
+        /// Cancel button handler
+        let cancelActionHandler = { (action: UIAlertAction) in
+
+            print("Cancel button tapped!")
+        }
+
+        /// Cancel action
+        let cancelAction = UIAlertAction(
+            title: "Cancel",
+            style: .cancel,
+            handler: cancelActionHandler
+        )
+
+        /// Alert presentation
+        let alertPresentation = BCAlertPresentation(
+            title: "Greeting",
+            message: "Hi there, how are you today?",
+            actions: [okAction, cancelAction]
+        )
+
+        alertPresenter.showAlert(
+            from: self,
+            presentation: alertPresentation
+        )
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
